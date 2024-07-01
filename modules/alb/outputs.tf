f59197b4e8e116257ea8e451ef3efea08d2b540b
emerjayd <mrj.d@mail.com>
2024-07-01 11:52:04 +0100

    update

--- modules/alb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.app_alb.dns_name
-}
-
-output "alb_arn" {
-  description = "The ARN of the ALB"
-  value       = aws_lb.app_alb.arn
-}
--- modules/ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_ids" {
-  value = aws_instance.app_server[*].id
-}
